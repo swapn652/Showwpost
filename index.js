78f16b167de403d5730901757145aa28e486615b
swapn_652 <swapnilpant0652@gmail.com>
2023-05-06 19:51:19 +0530

    pushing project

---
+++ index.js
@@ -0,0 +1,54 @@
+function getCurrentUrl() {
+    return new Promise((resolve, reject) => {
+        //Chrome extension API
+        // The object passed as the first argument to chrome.tabs.query() specifies that it should return information about the currently active tab in the currently active window, by setting the active and currentWindow properties to true.
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            if (tabs.length === 0) {
+                reject("No active tabs found.");
+            } else {
+                resolve(tabs[0].url);
+            }
+        });
+    });
+}
+
+getCurrentUrl()
+    .then(url => {
+        const btn = document.getElementById('btn')
+
+        const postThread = async (title, message, callback) => {
+            try {
+                const response = await fetch('https://cache.showwcase.com/threads', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'x-api-key': 'bd50a8beaa334573def7c4840da2c677d010bbae517238bdf4'
+                    },
+                    body: JSON.stringify({
+                        title: title,
+                        message: message
+                    })
+                });
+                console.log(response.data);
+                callback()
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        btn.addEventListener('click', () => {
+            const messageArea = document.getElementById('message')
+            let message = messageArea.value
+            const titleArea = document.getElementById('title')
+            const title = titleArea.value
+            message = message+'\n   '+url
+            postThread(title, message, ()=>{
+                window.close()
+            });
+        })
+    })
+    .catch(error => {
+        console.error(error)
+    });
+
+
